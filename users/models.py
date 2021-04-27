cd156bc1293aa47f31250dfe2fc7fe03a9d9b5b9
JeronimoMendes <jeronimo.m.mendes@gmail.com>
2021-04-27 19:30:58 +0100

    Create Profile when new User is created

--- users/models.py
+++ users/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from exams.models import Answer
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
 # Create Profile model.
+
+MATH, PHYSICS = "Matemática", "Física-Química"
 
 SUBJECTS = (
    (MATH, "Matemática"),
@@ -13,7 +17,7 @@ SUBJECTS = (
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    subjects = models.ManyToManyField(SubjectInfo)
+    subjects = models.ManyToManyField("SubjectInfo")
 
 
 class AnswerInfo(models.Model):
@@ -24,5 +28,16 @@ class AnswerInfo(models.Model):
 class SubjectInfo(models.Model):
     subject = models.CharField(max_length=50, null=False, choices=SUBJECTS)
     examCounter = models.IntegerField(default=0)
-    wrongAnswers = models.ManyToManyField(AnswerInfo, blank=True)
-    correctAnswers = models.ManyToManyField(AnswerInfo, blank=True)
+    wrongAnswers = models.ManyToManyField("AnswerInfo", blank=True, related_name="wrongAnswers")
+    correctAnswers = models.ManyToManyField("AnswerInfo", blank=True, related_name="correctAnswers")
+
+
+# When a new User object is created, a Profile is attached
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
