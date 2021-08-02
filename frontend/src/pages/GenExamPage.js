058457c7e3dbd919e66923a13435b243c37728d1
JeronimoMendes <jeronimo.m.mendes@gmail.com>
2021-08-02 01:53:45 +0100

    Turn off switchs if checkboxes are On

--- frontend/src/pages/GenExamPage.js
+++ frontend/src/pages/GenExamPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
 	Typography,
 	Grid,
@@ -13,36 +13,97 @@ import {
 import { createExam } from "../api";
 
 const GenExamPage = () => {
-
-	const [options, setOptions] = useState({
-		randomSubSubject: true,
+	const [dictSubSubjects, setDictSubSubjects] = useState({
 		geometry: false,
 		imaginary: false,
 		statistics: false,
-		probability: false,
-		randomGrade: true,
+		probability: false
+	})
+
+	const [dictYears, setDictYear] = useState({
 		tenthGrade: false,
 		eleventhGrade: false,
 		twelfthGrade: false
 	})
 
+	const [options, setOptions] = useState({
+		randomSubSubject: true,
+		randomGrade: true
+	})
 
-	const handleChange = (event) => {		
-		setOptions({...options, [event.target.name]: event.target.checked});
+	const baseSubSubjects = {
+		geometry: false,
+		imaginary: false,
+		statistics: false,
+		probability: false
 	}
+
+
+	const baseYear = {
+		tenthGrade: false,
+		eleventhGrade: false,
+		twelfthGrade: false
+	}
+
+
+	const resetDictYear = () => {
+		setDictYear(baseYear)
+	}
+
+
+	const resetDictSubSubjects = () => {
+		setDictSubSubjects(baseSubSubjects);
+	}
+
+
+	const handleChangeYear = (event) => {
+		console.log(event)
+		if (options.randomGrade) setOptions({...options, "randomGrade": false});
+		setDictYear({...dictYears, [event.target.name]: event.target.checked});
+	}
+
+
+	const handleChangeSubSubjects = (event) => {
+		if (options.randomSubSubject) setOptions({...options, "randomSubSubject": false});
+		setDictSubSubjects({...dictSubSubjects, [event.target.name]: event.target.checked});
+	}
+
+
+	const handleChangeRandomSubSubject = (event) => {
+		setOptions({...options, [event.target.name]: event.target.checked});
+
+		if (!options.randomSubSubject) {
+			resetDictSubSubjects();
+		}
+	}
+
+
+	const handleChangeRandomGrade = (event) => {
+		setOptions({...options, [event.target.name]: event.target.checked});
+
+		if (!options.randomGrade) {
+			resetDictYear();
+		}
+	}
+
 
 
 	const handleClick = () => {
 		const subSubjects = [
-			options.geometry ? "Geometria" : "none",
-			options.imaginary ?  "Imaginários" : "none",
+			dictSubSubjects.geometry ? "Geometria" : "none",
+			dictSubSubjects.imaginary ?  "Imaginários" : "none",
 		];
+
+		let year = 0;
+		if (options.tenthGrade) year = 10;
+		if (options.eleventhGrade) year = 11;
+		if (options.twelfthGrade) year = 12;
 
 		createExam({
 			subject: "math",
 			randomSubSubject: options.randomSubSubject,
 			subSubjects: subSubjects,
-			year: 0
+			year: year
 		}, (res) => {
 			window.location.href = "/exame/" + res.data.id;
 		})
@@ -58,11 +119,11 @@ const GenExamPage = () => {
 				<FormControl>
 					<FormLabel>Escolhe os temas</FormLabel>
 					<FormGroup>
-						<FormControlLabel control={<Checkbox checked={options.randomSubSubject} onChange={handleChange} name="randomSubSubject"/>} label="Aleatório"/>
-						<FormControlLabel control={<Switch checked={options.geometry} onChange={handleChange} name="geometry"/>} label="Geometria"/>
-						<FormControlLabel control={<Switch checked={options.imaginary} onChange={handleChange} name="imaginary"/>} label="Imaginários"/>
-						<FormControlLabel control={<Switch checked={options.statistics} onChange={handleChange} name="statistics"/>} label="Estatística"/>
-						<FormControlLabel control={<Switch checked={options.probability	} onChange={handleChange} name="probability"/>} label="Probabilidades"/>
+						<FormControlLabel control={<Checkbox checked={options.randomSubSubject} onChange={handleChangeRandomSubSubject} name="randomSubSubject"/>} label="Aleatório"/>
+						<FormControlLabel control={<Switch checked={dictSubSubjects.geometry && !options.randomSubSubject} onChange={handleChangeSubSubjects} name="geometry"/>} label="Geometria"/>
+						<FormControlLabel control={<Switch checked={dictSubSubjects.imaginary && !options.randomSubSubject} onChange={handleChangeSubSubjects} name="imaginary"/>} label="Imaginários"/>
+						<FormControlLabel control={<Switch checked={dictSubSubjects.statistics && !options.randomSubSubject} onChange={handleChangeSubSubjects} name="statistics"/>} label="Estatística"/>
+						<FormControlLabel control={<Switch checked={dictSubSubjects.probability && !options.randomSubSubject} onChange={handleChangeSubSubjects} name="probability"/>} label="Probabilidades"/>
 					</FormGroup>
 				</FormControl>
 			</Grid>
@@ -70,10 +131,10 @@ const GenExamPage = () => {
 				<FormControl>
 					<FormLabel>Escolhe os anos</FormLabel>
 					<FormGroup>
-						<FormControlLabel control={<Checkbox checked={options.randomGrade} onChange={handleChange} name="randomGrade"/>} label="Aleatório"/>
-						<FormControlLabel control={<Switch checked={options.tenthGrade} onChange={handleChange} name="tenthGrade"/>} label="10º"/>
-						<FormControlLabel control={<Switch checked={options.eleventhGrade} onChange={handleChange} name="eleventhGrade"/>} label="11º"/>
-						<FormControlLabel control={<Switch checked={options.twelfthGrade} onChange={handleChange} name="twelfthGrade"/>} label="12º"/>
+						<FormControlLabel control={<Checkbox checked={options.randomGrade} onChange={handleChangeRandomGrade} name="randomGrade"/>} label="Aleatório"/>
+						<FormControlLabel control={<Switch checked={dictYears.tenthGrade} onChange={handleChangeYear} name="tenthGrade"/>} label="10º"/>
+						<FormControlLabel control={<Switch checked={dictYears.eleventhGrade} onChange={handleChangeYear} name="eleventhGrade"/>} label="11º"/>
+						<FormControlLabel control={<Switch checked={dictYears.twelfthGrade} onChange={handleChangeYear} name="twelfthGrade"/>} label="12º"/>
 					</FormGroup>
 				</FormControl>
 			</Grid>
