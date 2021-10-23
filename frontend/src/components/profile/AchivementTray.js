443607fcc2ce344c18549955b283c50965d2eb13
Miguel Dinis de Sousa <miguel.dinis.s@tecnico.ulisboa.pt>
2021-10-23 14:20:40 +0100

    Changed Dependecies to MIU Only, added some font responsiveness

--- frontend/src/components/profile/AchivementTray.js
+++ frontend/src/components/profile/AchivementTray.js
@@ -1,9 +1,11 @@
-import { Box, Grid, Container, Paper } from '@material-ui/core';
-
-import React from 'react';
+import {Grid, Typography, responsiveFontSizes, createTheme, MuiThemeProvider, Select, FormControl, MenuItem, InputLabel, OutlinedInput, colors} from '@material-ui/core';
+import {React, useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { green } from '@material-ui/core/styles/createPalette';
 import AchivementsOutter from '../../containers/AchivementsOutter';
+
+
+let theme = createTheme();
+theme = responsiveFontSizes(theme);
 
 /*
         xs, extra-small: 0px
@@ -12,29 +14,75 @@ import AchivementsOutter from '../../containers/AchivementsOutter';
         lg, large: 1200px
         xl, extra-large: 1536px 
 */
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+
+    },
+  },
+};
 const useStyles = makeStyles(theme => ({
 	outterbox: {
 		borderRadius : 15,
         backgroundColor: '#999999C7',
-        boxShadow: green,
 	},
 	innerbox: {
 		borderRadius : 16,
 		backgroundColor: '#CCC9C9',
 	},
+    select:{
+        backgroundColor:'#FFFFFF',
+        minWidth : 120,
+    }
 }))
+
+const subjects = ['Todas','Matemática','Física e Química'];
 
 
 function AchivementTray() {
 
     const classes = useStyles();
+    const [selectedOption, setSelectedOption] = useState(subjects[0]);  
+    
 
     return (
         <div>
 
-            <Grid container spacing={4}>
+            <Grid  className={classes.outterbox} container spacing={4}>
+
+                <Grid item xs={true} >
+                    <MuiThemeProvider theme = {theme}>
+                        <Typography  variant='h5'>
+                            Achivements
+                        </Typography>
+                    </MuiThemeProvider>
+                </Grid>
+
+                <Grid  item xs="auto">
+				<Select className={classes.select}  value={selectedOption} onChange={(event) => setSelectedOption(event.target.value)}
+                MenuProps = {MenuProps} label='Disciplinas'>
+                    {subjects.map((subject)=>
+                        <MenuItem value = {subject}>
+                                <Typography variant= 'subtitle1'>
+                                    {subject}
+                                </Typography>
+                        </MenuItem>
+                    )}
+				</Select>
+                
+                </Grid>
+                <Grid>
+                    {/* Dotted Line */}
+
+                </Grid>
                 <Grid item xs={12}>
-                <AchivementsOutter/>
+                   
+                    <AchivementsOutter class={classes.innerbox}></AchivementsOutter>
                 </Grid>
             </Grid>
                 
--- frontend/src/containers/AchivementsOutter.js
+++ frontend/src/containers/AchivementsOutter.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import {Grid} from '@material-ui/core';
-import Dropdown from 'react-dropdown';
+import {Grid, Typography} from '@material-ui/core';
 
-function AchivementsOutter() {
-    const subjects=["Matemática", "Física e Química", "Todas as disciplinas"];
-    const defaultOption = subjects[subjects.length - 1]
+
+function AchivementsOutter(props) {
+
 
     return (
-        <div>
-            <Grid container spacing={4}>
-
-                <Grid item xs={10}>
-                </Grid>
-
-                <Grid item xs="auto">
-
-                    <Dropdown options={subjects} onChange={null} value={defaultOption} placeholder="Filtra por Disciplina" />
-                
-                </Grid>
-            </Grid>
+        <div className = {props.class}>
+            <Typography>
+                HelloWorld
+            </Typography>
         </div>
     )
 }
