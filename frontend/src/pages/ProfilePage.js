d8481b49a4d3944999c7af1036fa2be2cc656638
Jeronimo Mendes <jeronimo.m.mendes@gmail.com>
2021-11-20 18:35:05 +0000

    Define page structure

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -1,13 +1,41 @@
 import React from "react";
 import {
 	Grid,
+	Paper
 } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+	page: {
+		marginTop: "80px"
+	}
+}));
 
 const ResultsPage = (props) => {
+	const classes = useStyles();
 
 	return (
-		<Grid container align="center" spacing={4} xs={12}>
-			
+		<Grid container align="center" spacing={4} xs={12} className={classes.page} >
+			<Grid item xs={4}> {/* General Info */}
+				<Paper> {/* delete this component when implementing */}
+					This is where the General info pannel will be
+				</Paper>
+			</Grid>
+			<Grid item xs={8}> {/* XP Graph */}
+				<Paper> {/* delete this component when implementing */}
+					This is where the XP Graph pannel will be
+				</Paper>
+			</Grid>
+			<Grid item xs={4}> {/* Subject info */}
+				<Paper> {/* delete this component when implementing */}
+					This is where the subject info pannel will be
+				</Paper>
+			</Grid>
+			<Grid item xs={8}> {/* Subject Achievements Info */}
+				<Paper> {/* delete this component when implementing */}
+					This is where the subject achievements pannel will be
+				</Paper>	
+			</Grid>
 		</Grid>
 	);
 };
