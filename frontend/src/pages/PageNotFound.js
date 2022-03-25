125b87b66a7c517929e6ab520f53ac421c1136f5
Jerónimo Mendes <39437433+JeronimoMendes@users.noreply.github.com>
2022-03-25 17:42:11 +0000

    Add 404 page (#124)
    * Add route for page not found
    
    * Add page not found
    
    * Add action to click
    
    * Fix react 404 routing

---
+++ frontend/src/pages/PageNotFound.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import NormalButton from '../components/buttons/NormalButton'
+import { makeStyles, Grid } from '@material-ui/core'
+import { ReactComponent as Svg1 } from "../assets/404(1).svg"
+import { ReactComponent as Svg2 } from "../assets/404(2).svg"
+import { ReactComponent as Svg3 } from "../assets/404(3).svg"
+
+const useStyles = makeStyles((theme) => ({
+	svg: {
+		width: 650,
+		marginBottom: "3rem"
+	},
+}))
+
+const SVGS = [Svg1, Svg2, Svg3] 
+
+const PageNotFound = () => {
+	const classes = useStyles();
+
+	// pick a random svg
+	const RandomSVG = SVGS[Math.floor(Math.random() * SVGS.length)];
+
+	const handleClick = () => {
+		window.history.back()
+	}
+
+	return (
+		<Grid container className={classes.container} justify="center" align="center">
+			<Grid item xs={12}>
+				<RandomSVG className={classes.svg}/>
+			</Grid>
+			<Grid item>
+				<NormalButton onClick={handleClick} className={classes.button} text="Voltar atrás" fontSize={50} scale={1.015}/>
+			</Grid>
+		</Grid>
+	)
+}
+
+export default PageNotFound;
