3c24f5e97adaf4b9e3a78e2b45b25021e5b81911
Jeronimo Mendes <jeronimo.m.mendes@gmail.com>
2022-04-08 23:56:27 +0100

    Refactor `MenuCircular`

--- frontend/src/components/MenuCircular/MenuCircular.js
+++ frontend/src/components/MenuCircular/MenuCircular.js
@@ -1,6 +1,6 @@
 import { Planet } from 'react-planet';
 import React from "react";
-import { IconButton } from '@mui/material'
+import { IconButton, Typography } from '@mui/material'
 import AvatarUser from '../avatar/AvatarUser';
 import makeStyles from '@mui/styles/makeStyles';
 import SchoolIcon from '@mui/icons-material/School';
@@ -8,7 +8,6 @@ import PersonIcon from '@mui/icons-material/Person';
 import AssessmentIcon from '@mui/icons-material/Assessment';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import { logOut } from '../../api';
-
 
 const handleLogout = () => {
 	//handleClose();
@@ -20,7 +19,20 @@ const useStyles = makeStyles(theme => ({
     avatar: {
         width: 80,
         height: 80
-    },}))
+    },
+	iconButton: {
+		height: 40,
+		width: 40,
+		borderRadius: '50%',
+		backgroundColor: theme.palette.secondary.main,
+		color:'#FFFFFF',
+		display: 'flex',
+		alignItems: 'center',
+		'&:hover': {
+			color: theme.palette.primary.main
+		}
+	}
+}))
 
 const MenuCircular = (props) => {
 	
@@ -30,101 +42,63 @@ const MenuCircular = (props) => {
 	return (
         <div style={{marginTop: "-2.5rem"}}>
 			<Planet
-		centerContent={
-			<div style={{
-						borderRadius: '50%',
-						backgroundColor: '#1da8a4',
-					}}
-			>	
-				<AvatarUser className={classes.avatar} user={props.user} />
-			
-			</div>
-			
-		}
-		autoClose
-		hideOrbit
-		orbitRadius={90}
-		tension={600}
-		friction={25}
-	>    
+				centerContent={
+					<div 
+						style={{
+							borderRadius: '50%',
+							backgroundColor: '#1da8a4',
+						}}
+					>	
+						<AvatarUser className={classes.avatar} user={props.user} />
+					</div>
+				}
+				autoClose
+				hideOrbit
+				orbitRadius={90}
+				tension={600}
+				friction={25}
+			>    
 
+			<IconButton href="" title="Logout" onClick={handleLogout} size="large" className={classes.iconButton}>
+				<ExitToAppIcon/>
+			</IconButton> 
 
-		<IconButton href="" title="Logout" onClick={handleLogout} size="large"><div
-			style={{
-				height: 40,
-				width: 40,
-				borderRadius: '50%',
-				backgroundColor: '#EB5757',
-				color:'#FFFFFF',
-				display: 'flex',
-				alignItems: 'center',
-			}}>
-			<span style={{color:'#EB5757'}}>-</span><ExitToAppIcon/>
-		</div></IconButton> 
+			<div /> 
 
-		<div /> 
+			<IconButton href="/leaderboards" title="Leaderboards" size="large" className={classes.iconButton}>
+				<AssessmentIcon/>
+			</IconButton>
 
-		<IconButton href="/leaderboards" title="Leaderboards" size="large"><div
-			style={{
-				height: 40,
-				width: 40,
-				borderRadius: '50%',
-				backgroundColor: '#EB5757',
-				display: 'flex',
-				alignItems: 'center',
-				
-			}}>
-			<span style={{color:'#EB5757'}}>-</span><AssessmentIcon style={{fill: "white"}}/>
-		</div></IconButton>
-
-		<div /> 
-	
-		<IconButton href="/exames" title="Realizar Exames" size="large"><div 
-			style={{
-				height: 40,
-				width: 40,
-				borderRadius: '50%',
-				backgroundColor: '#EB5757',
-				color:'#FFFFFF',
-				display: 'flex',
-				alignItems: 'center',
-			}}>
-			<span style={{color:'#EB5757'}}>-</span><SchoolIcon/>
-		</div></IconButton>
-
-		<div /> 
-
-		<IconButton href="/perfil" title="Perfil" size="large"><div
-			style={{
-				height: 40,
-				width: 40,
-				borderRadius: '50%',
-				backgroundColor: '#EB5757',
-				color:'#FFFFFF',
-				display: 'flex',
-				alignItems: 'center',
-			}}>
-			<span style={{color:'#EB5757'}}>-</span><PersonIcon/>
-		</div></IconButton>  
-		 
-		<div /> 
-		<div /> 
-		<div />
-		<div /> 
-		<div /> 
-		<div /> 
-		<div />
-		<div /> 
-		<div /> 
-		<div /> 
-		<div />
-		<div /> 
-		<div /> 
-		<div /> 
-		<div />
-		<div /> 
+			<div /> 
 		
-	</Planet>
+			<IconButton href="/exames" title="Realizar Exames" size="large" className={classes.iconButton}>
+					<SchoolIcon/>
+			</IconButton>
+
+			<div /> 
+
+			<IconButton href="/perfil" title="Perfil" size="large" className={classes.iconButton}>
+				<PersonIcon/>
+			</IconButton>  
+			
+			<div /> 
+			<div /> 
+			<div />
+			<div /> 
+			<div /> 
+			<div /> 
+			<div />
+			<div /> 
+			<div /> 
+			<div /> 
+			<div />
+			<div /> 
+			<div /> 
+			<div /> 
+			<div />
+			<div /> 
+			
+		</Planet>
 		</div>
     );	
 };
