cf2642b8dfe2b1b5d04a47ea4d8a5224a823ed6b
Jeronimo Mendes <jeronimo.m.mendes@gmail.com>
2022-04-19 22:37:27 +0100

    Create `config` file

---
+++ frontend/src/config.js
@@ -0,0 +1,24 @@
+const config = {
+	subjects: [
+		{
+			name: 'Matemática',
+			themes: [
+				'Geometria',
+				'Funções',
+				'Probabilidades e Cálculo Combinatório',
+				'Números complexos',
+				'Estatística'
+			]
+		},
+		{
+			name: 'Fisico-Química',
+			themes: [
+				'Geometria',
+				'Funções',
+				'Probabilidades e Cálculo Combinatório',
+				'Números complexos',
+				'Estatística'
+			]
+		}
+	]
+}
