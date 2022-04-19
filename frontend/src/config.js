1668c88f42eecd87dc7b8334ae4f2035eb67e265
Jeronimo Mendes <jeronimo.m.mendes@gmail.com>
2022-04-19 22:48:26 +0100

    Adapt `QuestionForm` to config file

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,23 +2,34 @@ const config = {
 	subjects: [
 		{
 			name: 'Matemática',
+			active: true,
 			themes: [
 				'Geometria',
 				'Funções',
 				'Probabilidades e Cálculo Combinatório',
 				'Números complexos',
 				'Estatística'
+			],
+			years: [
+				10,
+				11,
+				12
 			]
 		},
 		{
 			name: 'Fisico-Química',
+			active: false,
 			themes: [
-				'Geometria',
-				'Funções',
-				'Probabilidades e Cálculo Combinatório',
-				'Números complexos',
-				'Estatística'
+				'Mecânica',
+				'Eletricidade',
+				'Química'
+			],
+			years: [
+				10,
+				11
 			]
 		}
 	]
 }
+
+export default config;
