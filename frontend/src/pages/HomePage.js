8424d514ced56b790df2d7583dc683c9405ae7fe
Miguel Dinis de Sousa <miguel.dinis.s@tecnico.ulisboa.pt>
2022-05-21 00:09:01 +0100

    initial Grid structure for Home Page

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,22 +1,98 @@
-import React, { Component } from 'react';
-import QuestionPage from './QuestionPage';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from 'react';
+import { Grid, Paper, Typography } from '@mui/material';
+import makeStyles from '@mui/styles/makeStyles/';
+import theme from '../globalTheme';
+import IconButton from '../components/buttons/IconButton';
+import AbcIcon from '@mui/icons-material/Abc';
 
-export default class HomePage extends Component {
-	constructor(props) {
-		super(props);
-	}
+const useStyles = makeStyles(() => ({
+	paper: {
+		width: '100%',
+		height: '80vh',
+		borderRadius: 40,
+		border: '3px solid #D9D9D9',
+		boxShadow: '-6px 4px 6px rgba(0, 0, 0, 0.25)',
+	},
+	container: {
+		width: '100%',
+		height: '100%',
+		padding: '3rem',
+	},
+	icon: {
+		fontSize: 100,
+	},
+}));
 
-	render() {
-		return (
-			<Router>
-				<Switch>
-					<Route exact path='/test'>
-						<p>This is the home page</p>
-					</Route>
-					<Route path='/question/:id' component={QuestionPage} />
-				</Switch>
-			</Router>
-		);
-	}
-}
+export const HomePage = () => {
+	const classes = useStyles();
+	const icon = <AbcIcon className={classes.icon} />;
+
+	return (
+		<Paper theme={theme} className={classes.paper}>
+			<Grid
+				container
+				direction='column'
+				justifyContent='center'
+				alignItems='flex-start'
+				className={classes.container}
+				spacing={4}
+			>
+				<Grid item xs='auto'>
+					<Typography>text</Typography>
+				</Grid>
+
+				{/* Home Buttons */}
+				<Grid item xs='auto'>
+					<Grid
+						spacing={4}
+						container
+						direction='row'
+						justifyContent='flex-start'
+						alignItems='center'
+					>
+						<Grid item xs={4}>
+							<IconButton
+								direction='column'
+								fontSize={25}
+								text='Entrar'
+								onClick={null}
+								iconFirst={false}
+							>
+								{icon}
+							</IconButton>
+						</Grid>
+						<Grid item xs={4}>
+							<IconButton
+								direction='column'
+								fontSize={25}
+								text='Entrar'
+								onClick={null}
+								iconFirst={false}
+							>
+								{icon}
+							</IconButton>
+						</Grid>
+						<Grid item xs={4}>
+							<IconButton
+								direction='column'
+								fontSize={25}
+								text='Entrar'
+								onClick={null}
+								iconFirst={false}
+							>
+								{icon}
+							</IconButton>
+						</Grid>
+					</Grid>
+				</Grid>
+				<Grid item xs='auto'>
+					<Typography>text</Typography>
+				</Grid>
+
+				<Grid item xs='auto'>
+					<Typography>text</Typography>
+				</Grid>
+			</Grid>
+		</Paper>
+	);
+};
