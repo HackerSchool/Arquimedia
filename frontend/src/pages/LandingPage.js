619579bf6e38d864c3080aee03ec45632ca8e24a
Miguel Dinis de Sousa <miguel.dinis.s@tecnico.ulisboa.pt>
2022-05-21 17:29:23 +0100

    Added Routing to Dashboard

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Grid, Typography } from '@mui/material';
 import makeStyles from '@mui/styles/makeStyles';
 import { ReactComponent as Girl } from '../assets/girl_studying.svg';
 import { ReactComponent as Emoji } from '../assets/winking_emoji.svg';
 import NormalButton from '../components/buttons/NormalButton';
+import { userContext } from '../context/UserContextProvider';
+import { HomePage } from './HomePage';
 
 const useStyles = makeStyles(() => ({
 	mainBox: {
@@ -20,32 +22,39 @@ const useStyles = makeStyles(() => ({
 
 const LandingPage = () => {
 	const classes = useStyles();
+	const [user, loading] = useContext(userContext);
+
+	if (!loading && user !== null) {
+		return <HomePage />;
+	}
 
 	return (
-		<Grid
-			container
-			className={classes.mainBox}
-			xs={12}
-			justifyContent='center'
-			alignItems='center'
-			spacing={8}
-		>
-			<Grid item container xs={4} direction='column' align='center' spacing={6}>
-				<Grid item>
-					<Typography className={classes.slogan}>
-						Exames nacionais
-						<br />
-						<i>made easy</i> <Emoji />
-					</Typography>
+		!loading && (
+			<Grid
+				container
+				className={classes.mainBox}
+				xs={12}
+				justifyContent='center'
+				alignItems='center'
+				spacing={8}
+			>
+				<Grid item container xs={4} direction='column' align='center' spacing={6}>
+					<Grid item>
+						<Typography className={classes.slogan}>
+							Exames nacionais
+							<br />
+							<i>made easy</i> <Emoji />
+						</Typography>
+					</Grid>
+					<Grid item>
+						<NormalButton xs={6} text='Inscreve-te' href='/registar' fontSize={41} />
+					</Grid>
 				</Grid>
 				<Grid item>
-					<NormalButton xs={6} text='Inscreve-te' href='/registar' fontSize={41} />
+					<Girl className={classes.girl} />
 				</Grid>
 			</Grid>
-			<Grid item>
-				<Girl className={classes.girl} />
-			</Grid>
-		</Grid>
+		)
 	);
 };
 
