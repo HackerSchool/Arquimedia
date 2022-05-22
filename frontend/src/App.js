91384f0767f546bb009af3f9a7f6ca03d2302560
Jeronimo Mendes <jeronimo.m.mendes@gmail.com>
2022-05-22 14:09:19 +0100

    Add warning page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,10 +23,16 @@ import { AuthRoute } from './routes/AuthRoute.js';
 import { UserContextProvider } from './context/UserContextProvider.js';
 import { SnackbarProvider } from 'notistack';
 import { SettingsPage } from './pages/SettingsPage.js';
+import { MobileWarningPage } from './pages/MobileWarningPage.js';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 axios.defaults.headers.common['Authorization'] = localStorage.getItem('Authorization');
 
 function App() {
+	const matches = useMediaQuery('(max-width:1000px)');
+
+	if (matches) return <MobileWarningPage />;
+
 	return (
 		<UserContextProvider>
 			<StyledEngineProvider injectFirst>
