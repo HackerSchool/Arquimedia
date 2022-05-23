a1e2d1f20c1ac528d1138b1bb139692f38c09263
Jerónimo Mendes <39437433+JeronimoMendes@users.noreply.github.com>
2022-05-23 20:28:03 +0100

    Add Redis to Docker (#175)
    * Add Redis to Docker
    
    * Configure django with redis server

--- backend/Arquimedia/settings.py
+++ backend/Arquimedia/settings.py
@@ -175,8 +175,8 @@ MEDIA_URL = "/images/"
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Celery settings
-BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+BROKER_URL = os.getenv("REDIS_URL")
+CELERY_RESULT_BACKEND = os.getenv("REDIS_URL")
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
