45e496befe640d00eee1ea55e9cba478edaf1c46
Jeronimo Mendes <jeronimo.m.mendes@gmail.com>
2022-05-24 23:37:31 +0100

    Add `STATIC_ROOT` to django settings

--- backend/Arquimedia/settings.py
+++ backend/Arquimedia/settings.py
@@ -151,6 +151,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
