6c3aaed820b6e72afc6c37e8aa08a232c946d5a8
afonsofsdomingues <afonso.silva.domingues@tecnico.ulisboa.pt>
2022-05-25 22:02:12 +0100

    Squashed commit of the following:
    commit ca780dab58d297d0c44c319ef115b05a71ab36d6
    Author: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Date:   Wed May 25 11:46:19 2022 +0100
    
        Bump pyjwt from 2.1.0 to 2.4.0 in /backend (#185)
    
        Bumps [pyjwt](https://github.com/jpadilla/pyjwt) from 2.1.0 to 2.4.0.
        - [Release notes](https://github.com/jpadilla/pyjwt/releases)
        - [Changelog](https://github.com/jpadilla/pyjwt/blob/master/CHANGELOG.rst)
        - [Commits](https://github.com/jpadilla/pyjwt/compare/2.1.0...2.4.0)
    
        ---
        updated-dependencies:
        - dependency-name: pyjwt
          dependency-type: direct:production
        ...
    
        Signed-off-by: dependabot[bot] <support@github.com>
    
        Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    commit 19c0431783b64d4419ea56997b930e11325eac01
    Author: Jerónimo Mendes <39437433+JeronimoMendes@users.noreply.github.com>
    Date:   Tue May 24 22:52:32 2022 +0100
    
        Fix django for production (#182)
    
        * Add gunicorn to requirements
    
        * Add logging
    
    commit 5b9050dba66067257a70bda1bddd25c37e94f606
    Author: Miguel Dinis <80652363+LordOfTheNeverThere@users.noreply.github.com>
    Date:   Tue May 24 22:40:43 2022 +0100
    
        Fix Exam's questions and answers sizes (#180)
    
        * Is it fixed?
    
        * Fixed Overextension of QuestionBox on Exam Page
    
        * Fixed The height and Streching of Question, when there is alot of text
    
        * Question Submission Page Improvement
    
        * Added Spacing tto the QuestionForm TextFields
    
        * Made Question in Approval appear with the correct size
    
        * Updated the use effect for improving question text resizing
    
        * Changed Answers minWidth

--- backend/Arquimedia/settings.py
+++ backend/Arquimedia/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 from pathlib import Path
+import logging.config
 import os
 from dotenv import load_dotenv
 from celery.schedules import crontab
@@ -187,3 +188,33 @@ CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'Europe/Lisbon'
+
+# Logging Configuration
+
+# Clear prev config
+LOGGING_CONFIG = None
+
+# Get loglevel from env
+LOGLEVEL = os.getenv('DJANGO_LOGLEVEL', 'info').upper()
+
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'console': {
+            'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'console',
+        },
+    },
+    'loggers': {
+        '': {
+            'level': LOGLEVEL,
+            'handlers': ['console',],
+        },
+    },
+})
