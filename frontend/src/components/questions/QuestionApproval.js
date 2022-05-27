e71369c94033d2ee1fbb3c2fbe22364740606da6
Jerónimo Mendes <39437433+JeronimoMendes@users.noreply.github.com>
2022-05-28 00:35:18 +0100

    Show question information (#200)

--- frontend/src/components/questions/QuestionApproval.js
+++ frontend/src/components/questions/QuestionApproval.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, IconButton } from '@mui/material';
+import { Grid, IconButton, Paper, Typography } from '@mui/material';
 import makeStyles from '@mui/styles/makeStyles';
 import ClearIcon from '@mui/icons-material/Clear';
 import CheckIcon from '@mui/icons-material/Check';
@@ -9,6 +9,9 @@ import Question from '../questions/Question';
 const useStyles = makeStyles(() => ({
 	question: {
 		width: '100%',
+	},
+	questionInfoLabel: {
+		fontWeight: 'bold',
 	},
 }));
 
@@ -43,6 +46,48 @@ const QuestionApproval = ({ question, callBack }) => {
 				<IconButton onClick={accept} size='large'>
 					<CheckIcon color='primary' />
 				</IconButton>
+			</Grid>
+
+			<Grid item xs={12} style={{ margin: '4rem' }} justifyContent='space-around'>
+				{' '}
+				{/* Information */}
+				<Paper style={{ padding: '1rem' }}>
+					<Grid container spacing={4}>
+						<Grid item xs={3}>
+							<Typography>
+								Disciplina:{' '}
+								<span className={classes.questionInfoLabel}>
+									{question.subject}
+								</span>
+							</Typography>
+						</Grid>
+						<Grid item xs={3}>
+							<Typography>
+								Tema:{' '}
+								<span className={classes.questionInfoLabel}>
+									{question.subsubject}
+								</span>
+							</Typography>
+						</Grid>
+						<Grid item xs={3}>
+							<Typography>
+								Ano:{' '}
+								<span className={classes.questionInfoLabel}>{question.year}</span>
+							</Typography>
+						</Grid>
+						<Grid item xs={3}>
+							<Typography>
+								Fonte:{' '}
+								<span className={classes.questionInfoLabel}>{question.source}</span>
+							</Typography>
+						</Grid>
+						<Grid item xs={3}>
+							<Typography>
+								ID: <span className={classes.questionInfoLabel}>{question.id}</span>
+							</Typography>
+						</Grid>
+					</Grid>
+				</Paper>
 			</Grid>
 		</Grid>
 	);
