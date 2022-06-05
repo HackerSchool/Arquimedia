6d2096d5919da2164f83c5be0464a3db1cf13ded
Jerónimo Mendes <39437433+JeronimoMendes@users.noreply.github.com>
2022-06-05 14:38:00 +0100

    Add resource system (#251)
    * Create `Resource` model
    
    * Create `Resource` serializer
    
    * Create POST endpoint
    
    * Add DELETE endpoint
    
    * Add permission class

--- backend/api/views.py
+++ backend/api/views.py
@@ -10,7 +10,7 @@ from rest_framework.response import Response
 import random
 from rest_framework.parsers import FileUploadParser, MultiPartParser, FormParser, JSONParser
 from django.shortcuts import get_object_or_404, get_list_or_404
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 import datetime
 from rest_auth.registration.serializers import VerifyEmailSerializer
 from allauth.account.models import EmailAddress
@@ -574,4 +574,27 @@ class DeleteAccount(APIView):
 			{"detail": ("Account has been deleted")},
 			status=status.HTTP_200_OK
 		)
+
+class ResourceView(APIView):
+	permission_classes = [IsAdminUser]
+	
+	def post(self, request, id):
+		serializer = ResourceSerializer(data=request.data)
+		serializer.is_valid(raise_exception=True)
+
+		resource = Resource.objects.create(
+			description=serializer.data.get("description"),
+			url=serializer.data.get("url"),
+			type=serializer.data.get("type"),
+			question=get_object_or_404(Question, id=id)
+		)
+
+		return Response(ResourceSerializer(resource).data, status=status.HTTP_201_CREATED)
+
+	def delete(self, request, id):
+		resource = get_object_or_404(Resource, id=id)
+		resource.delete()
+
+		return Response(status=status.HTTP_200_OK)
+
 
