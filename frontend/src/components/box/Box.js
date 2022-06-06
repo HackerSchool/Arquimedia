8930deecc9138cf1e38a3e91e08bc3d8919a15f3
Jeronimo Mendes <jeronimo.m.mendes@gmail.com>
2022-06-06 15:46:26 +0100

    Create new box component

---
+++ frontend/src/components/box/Box.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Paper } from '@mui/material';
+import makeStyles from '@mui/styles/makeStyles';
+import theme from '../../globalTheme';
+
+const useStyles = makeStyles(() => ({
+	box: {
+		border: '2px solid',
+		borderRadius: 20,
+		borderColor: theme.palette.grey.primary,
+		boxShadow: '-6px 7px 16px rgba(0, 0, 0, 0.25)',
+	},
+}));
+
+const Box = (props) => {
+	const classes = useStyles();
+
+	return <Paper className={classes.box}>{props.children}</Paper>;
+};
+
+export default Box;
