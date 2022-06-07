5c7712a70406a2f1491586f991d12418ff27f406
afonsofsdomingues <afonso.silva.domingues@tecnico.ulisboa.pt>
2022-06-07 22:53:20 +0100

    Noob mistakes solved

--- backend/Arquimedia/settings.py
+++ backend/Arquimedia/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.getenv("DJANGO_DEBUG", True) == "True")
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS")]
 
 
 # Application definition
