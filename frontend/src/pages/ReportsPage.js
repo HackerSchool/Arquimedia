e680d8b9eac6d0aac44a5476c8a91f833e26bca8
Miguel Dinis de Sousa <miguel.dinis.s@tecnico.ulisboa.pt>
2022-08-08 20:49:31 +0100

    Added all filtering functionality

--- frontend/src/pages/ReportsPage.js
+++ frontend/src/pages/ReportsPage.js
@@ -18,6 +18,7 @@ import {
 	Tooltip,
 	Select,
 	MenuItem,
+	TextField,
 } from '@mui/material';
 import FilterListIcon from '@mui/icons-material/FilterList';
 import { visuallyHidden } from '@mui/utils';
@@ -25,6 +26,9 @@ import { getReports } from '../api';
 import Loading from '../components/loading/Loading';
 import Report from '../components/report/Report';
 import isUnique from '../utils/isUnique';
+import SearchIcon from '@mui/icons-material/Search';
+import RefreshIcon from '@mui/icons-material/Refresh';
+import CloseIcon from '@mui/icons-material/Close';
 
 function descendingComparator(a, b, orderBy) {
 	// We assume that values are descending
@@ -141,18 +145,48 @@ EnhancedTableHead.propTypes = {
 };
 
 const EnhancedTableToolbar = (props) => {
+	const [reportIDs, setReportIDs] = useState([]);
+	const [questionIDs, setQuestionIDs] = useState([]);
 	const [types, setTypes] = useState([]);
+	const [reportID, setReportID] = useState('none');
+	const [questionID, setQuestionID] = useState('none');
 	const [chosenType, setChosenType] = useState('none');
+	const [bodyQuery, setBodyQuery] = useState('');
+	const [filtering, setFiltering] = useState(false);
 
 	useEffect(() => {
+		var reportIDs = props.rows.map((row) => row['id']);
+		var uniqueReportIDs = reportIDs.filter(isUnique);
+		setReportIDs(uniqueReportIDs);
+		var questionIDs = props.rows.map((row) => row['question']);
+		var uniqueQuestionIDs = questionIDs.filter(isUnique);
+		setQuestionIDs(uniqueQuestionIDs);
 		var types = props.rows.map((row) => row['type']);
 		var uniqueTypes = types.filter(isUnique);
 		setTypes(uniqueTypes);
 	}, []);
-
+	const handleReportIDChange = (event) => {
+		setReportID(event.target.value);
+	};
+	const handleQuestionIDChange = (event) => {
+		setQuestionID(event.target.value);
+	};
 	const handleErrorTypeChange = (event) => {
 		setChosenType(event.target.value);
-		props.onErrorTypeChange(event.target.value);
+	};
+	const handleBodyQueryChange = (event) => {
+		setBodyQuery(event.target.value);
+	};
+	const search = () => {
+		props.searchFunction(reportID, questionID, chosenType, bodyQuery);
+		setBodyQuery('');
+	};
+	const resetFilters = () => {
+		setReportID('none');
+		setQuestionID('none');
+		setChosenType('none');
+		setBodyQuery('');
+		props.resetFilters();
 	};
 
 	return (
@@ -165,26 +199,88 @@ const EnhancedTableToolbar = (props) => {
 			<Typography sx={{ flex: '1 1 100%' }} variant='h6' id='tableTitle' component='div'>
 				Reports
 			</Typography>
-			<Select
-				labelId='errorType'
-				id='errorType'
-				value={chosenType}
-				onChange={handleErrorTypeChange}
-				disableUnderline
-			>
-				{types.map((type, index) => (
-					<MenuItem value={type} key={index}>
-						{type}
-					</MenuItem>
-				))}
-				<MenuItem value={'none'}>Filter by Error Type</MenuItem>
-			</Select>
 
-			<Tooltip title='Filter list'>
-				<IconButton>
-					<FilterListIcon />
-				</IconButton>
-			</Tooltip>
+			{filtering ? (
+				<>
+					{' '}
+					<Select
+						labelId='ReportID'
+						id='ReportID'
+						value={reportID}
+						onChange={handleReportIDChange}
+					>
+						{reportIDs.map((type, index) => (
+							<MenuItem value={type} key={index}>
+								{type}
+							</MenuItem>
+						))}
+						<MenuItem value={'none'}>Todos os ReportIDs</MenuItem>
+					</Select>
+					<Select
+						labelId='QuestionID'
+						id='QuestionID'
+						value={questionID}
+						onChange={handleQuestionIDChange}
+						style={{ margin: '1rem' }}
+					>
+						{questionIDs.map((type, index) => (
+							<MenuItem value={type} key={index}>
+								{type}
+							</MenuItem>
+						))}
+						<MenuItem value={'none'}>Todos as questões</MenuItem>
+					</Select>
+					<Select
+						labelId='errorType'
+						id='errorType'
+						value={chosenType}
+						onChange={handleErrorTypeChange}
+					>
+						{types.map((type, index) => (
+							<MenuItem value={type} key={index}>
+								{type}
+							</MenuItem>
+						))}
+						<MenuItem value={'none'}>Filtre por tipo de erro</MenuItem>
+					</Select>
+					<TextField
+						style={{ margin: '1rem' }}
+						variant='standard'
+						fullWidth
+						value={bodyQuery}
+						name='bodyQuery'
+						placeholder='Pesquise por descrição....'
+						label=''
+						onChange={handleBodyQueryChange}
+						InputProps={{
+							disableUnderline: true,
+						}}
+					/>
+					<Tooltip title='Search'>
+						<IconButton onClick={search}>
+							<SearchIcon />
+						</IconButton>
+					</Tooltip>
+					<Tooltip title='Reset Filters'>
+						<IconButton onClick={resetFilters}>
+							<RefreshIcon />
+						</IconButton>
+					</Tooltip>
+				</>
+			) : null}
+			{filtering ? (
+				<Tooltip title='Close filters tab'>
+					<IconButton onClick={() => setFiltering(!filtering)}>
+						<CloseIcon />
+					</IconButton>
+				</Tooltip>
+			) : (
+				<Tooltip title='Open filters tab'>
+					<IconButton onClick={() => setFiltering(!filtering)}>
+						<FilterListIcon />
+					</IconButton>
+				</Tooltip>
+			)}
 		</Toolbar>
 	);
 };
@@ -226,7 +322,8 @@ const ReportsPage = () => {
 		setRowsPerPage(parseInt(event.target.value, 10));
 		setPage(0);
 	};
-	const filterErrorType = (type) => {
+
+	/* 	const filterErrorType = (type) => {
 		if (type === 'none' && rows !== originalRows) {
 			setRows(originalRows);
 		} else {
@@ -235,13 +332,62 @@ const ReportsPage = () => {
 			setRows(newReports);
 		}
 	};
+		const search = (bodyQuery) => {
+		if (bodyQuery && bodyQuery.length > 0) {
+			// if it exists
+
+			var reports = originalRows;
+			var newReports = reports
+				.filter((report) => report['body']) //filter only rows with a body !== null
+				.filter((report) => {
+					return report['body'].toLowerCase().includes(bodyQuery.toLowerCase());
+				}); //Search is case insentive
+
+			setRows(newReports);
+		} else {
+			setRows([]);
+		}
+	}; */
+
+	const search = (reportID, questionID, chosenType, bodyQuery) => {
+		var reports = originalRows;
+		console.log(reports);
+		if (reportID !== 'none') {
+			//The journey ends here
+			reports = reports.filter((report) => reportID === report['id']);
+		} else {
+			if (questionID !== 'none') {
+				reports = reports.filter((report) => questionID === report['question']);
+			}
+			if (chosenType !== 'none') {
+				reports = reports.filter((report) => chosenType === report['type']);
+			}
+			if (bodyQuery.length > 0) {
+				reports = reports
+					.filter((report) => report['body']) //filter only rows with a body !== null
+					.filter((report) => {
+						return report['body'].toLowerCase().includes(bodyQuery.toLowerCase());
+					}); //Search is case insentive
+				console.log(reports);
+			}
+		}
+
+		setRows(reports);
+	};
+	const resetRows = () => {
+		setRows(originalRows);
+	};
 
 	if (loading) return <Loading />;
 
 	return (
 		<Box sx={{ width: '100%' }}>
 			<Paper sx={{ width: '100%', mb: 2 }}>
-				<EnhancedTableToolbar rows={rows} onErrorTypeChange={filterErrorType} />
+				<EnhancedTableToolbar
+					rows={rows}
+					searchFunction={search}
+					resetFilters={resetRows}
+				/>
 				<TableContainer>
 					<Table sx={{ minWidth: 750 }} aria-labelledby='tableTitle' size={'medium'}>
 						<colgroup>
