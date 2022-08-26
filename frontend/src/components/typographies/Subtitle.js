df95ad09949ced82d55ed6085a27fc928c6ba1ce
Miguel Dinis de Sousa <miguel.dinis.s@tecnico.ulisboa.pt>
2022-08-26 23:07:23 +0100

    Made changes to the Drawers text along with two new components to systematize our UI

---
+++ frontend/src/components/typographies/Subtitle.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+import responsiveWidth from '../../hooks/responsiveWidth';
+import theme from '../../globalTheme';
+
+const Subtitle = (props) => {
+	const sxStyles = {
+		Subtitle: {
+			fontWeight: 'bold',
+			fontSize: responsiveWidth(undefined, 15, 35, 0.017),
+			color: theme.palette.secondary.main,
+		},
+	};
+	return (
+		<Typography sx={sxStyles.Subtitle} {...props}>
+			{props.children}
+		</Typography>
+	);
+};
+
+export default Subtitle;
