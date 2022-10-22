b179e3971a2c1c157c0cbd6707db803954fa965a
Jerónimo Mendes <jeronimo.m.mendes@gmail.com>
2022-10-23 00:26:51 +0100

    Add QuestionGroups to admin

--- backend/exams/admin.py
+++ backend/exams/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Exam, Question, Answer, Report
+from .models import Exam, Question, Answer, Report, QuestionGroup
 
 # Register your models here.
 admin.site.register(Exam)
 admin.site.register(Question)
 admin.site.register(Answer)
 admin.site.register(Report)
+admin.site.register(QuestionGroup)
