28689d7ea87ea36a14f64b181b9dd4fa51bd8fb4
afonsofsdomingues <afonso.silva.domingues@tecnico.ulisboa.pt>
2022-11-11 23:44:09 +0000

    Urls and admin site added

--- backend/exams/admin.py
+++ backend/exams/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Exam, Question, Answer, Report
+from .models import Exam, Question, FillInTheBlankQuestion, Answer, Report
 
 # Register your models here.
 admin.site.register(Exam)
 admin.site.register(Question)
+admin.site.register(FillInTheBlankQuestion)
 admin.site.register(Answer)
 admin.site.register(Report)
